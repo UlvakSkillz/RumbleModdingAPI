8e4f29ade2bb1cc824469cc9ccf9222449122e28
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2025-08-03 19:08:08 -0700

    3.6.1

--- RumbleModdingAPI/obj/Debug/net6.0/RumbleModdingAPI.AssemblyInfo.cs
+++ RumbleModdingAPI/obj/Debug/net6.0/RumbleModdingAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RumbleModdingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1bc836b356d8f626c37193b0f813ae55715e581")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca9f59268c92f90a369373a0f678f1f021f62886")]
 [assembly: System.Reflection.AssemblyProductAttribute("RumbleModdingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RumbleModdingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
